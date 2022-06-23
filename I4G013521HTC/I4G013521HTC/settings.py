ed2513229a7d683841b5490012f354ee4c2c3e71
Toheeb <aladetoheeb5@gmail.com>
2022-06-23 22:27:56 +0100

    Added templates

--- I4G013521HTC/I4G013521HTC/settings.py
+++ I4G013521HTC/I4G013521HTC/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'I4G013521HTC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
